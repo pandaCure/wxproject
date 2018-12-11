7fc181e8fd823e02bfe17e6cbf481977070f2970
Jazk-Z <zzyamor@gmail.com>
2018-12-11 23:57:11 +0800

    轮播图+七牛

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,39 @@
-// pages/index/home.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    swiperData: [
+      {
+        imgurl:'http://pjkva1akb.bkt.clouddn.com/swiper1.jpeg',
+        index:0
+      },
+      {
+        imgurl: 'http://pjkva1akb.bkt.clouddn.com/swiper2.jpeg',
+        index: 1
+      },
+      {
+        imgurl: 'http://pjkva1akb.bkt.clouddn.com/swiper3.jpeg',
+        index: 2
+      },
+      {
+        imgurl: 'http://pjkva1akb.bkt.clouddn.com/swiper4.jpeg',
+        index: 3
+      },
+      {
+        imgurl: 'http://pjkva1akb.bkt.clouddn.com/swiper5.jpeg',
+        index: 4
+      }
+    ],
+    tripData: [
+      {},
+      {},
+      {},
+      {}
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPullDownRefresh(){
+    console.log('refresh')
   }
 })
