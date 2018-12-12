6b3a6ede8449c4d0ba4720ff562537d5a63edb9f
pandaCure <zzyamor@163.com>
2018-12-12 14:28:06 +0800

    微信小程序修改UI

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 App({
-
-  /**
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
-   */
-  onLaunch: function () {
-    console.log(arguments)
+  data: {
+    appid: 'wx19fd18a09198044b',
+    appSecret: '36146f157a0dfc68b1776c9b1ddcca46'
   },
-
-  /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
-   */
-  onShow: function (options) {
-    console.log('onshow')
+  onLaunch() {
+    wx.login({
+      success: (loginRes) => {
+        if (loginRes && loginRes.code) {
+          const {
+            appid,
+            appSecret
+          } = this.data
+          this.getSessionKey(loginRes.code, appid, appSecret)
+        }
+      }
+    })
   },
-
-  /**
-   * 当小程序从前台进入后台，会触发 onHide
-   */
-  onHide: function () {
-    console.lof(onHide)
+  onShow() {
+    console.log(this.data)
   },
-
-  /**
-   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
-  onError: function (msg) {
-    console.log(onError)
+  getSessionKey(code, appid, appSecret) {
+    var opt = {
+      method: 'GET',
+      url: 'https://api.weixin.qq.com/sns/jscode2session',
+      data: {
+        appid: appid,
+        secret: appSecret,
+        js_code: code,
+        grant_type: 'authorization_code'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      dataType: "json",
+      success: function(response) {
+        var data = response.data;
+        if (!data.openid || !data.session_key || data.errcode) {
+          return {
+            result: -2,
+            errmsg: data.errmsg || '返回数据字段不完整'
+          }
+        } else {
+          return data
+        }
+      }
+    };
+    wx.request(opt);
   }
 })
