e2c02d3d4a26f94dccd1be81ec9e022d4c2a552d
Jazk-Z <zzyamor@gmail.com>
2018-12-14 08:27:14 +0800

    add pull data

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    swiperData: [
-      {
-        imgurl:'http://pjkva1akb.bkt.clouddn.com/swiper1.jpeg',
-        index:0
+    swiperData: [{
+        imgurl: 'http://pjkva1akb.bkt.clouddn.com/swiper1.jpeg',
+        index: 0
       },
       {
         imgurl: 'http://pjkva1akb.bkt.clouddn.com/swiper2.jpeg',
@@ -26,23 +25,89 @@ Page({
         index: 4
       }
     ],
-    tripData: [
-      {},
+    tripData: [{},
       {},
       {},
       {}
-    ]
+    ],
+    containerBottom: 0,
+    isReachBottom: false,
+    clientY: 0,
+    threshold: 100,
+    translate: '',
+    top: true
   },
-  onPullDownRefresh(){
-    console.log('refresh')
+  onPullDownRefresh() {
+
   },
-  getImageInfo ({ detail }) {
+  getImageInfo({
+    detail
+  }) {
     console.log(detail)
   },
-  onReachBottom () {
-    console.log(`页面上拉触底事件的处理函数`)
+  onReachBottom() {
+    this.setData({
+      isReachBottom: true
+    })
   },
-  onPageScroll () {
-    console.log(`页面滚动触发事件的处理函数`)
+  onPageScroll({
+    scrollTop
+  }) {
+    const {
+      isReachBottom,
+      top
+    } = this.data
+    if (isReachBottom && top) {
+      wx.pageScrollTo({
+        scrollTop
+      })
+      this.setData({
+        top: false
+      })
+    }
+  },
+  start(e) {
+    const {
+      isReachBottom
+    } = this.data
+    if (isReachBottom) {
+      const clientY = e.changedTouches[0].clientY;
+      this.setData({
+        clientY
+      })
+    }
+  },
+  move(e) {
+    const {
+      isReachBottom,
+      clientY,
+      threshold
+    } = this.data
+    if (isReachBottom) {
+      let nowClientY = e.changedTouches[0].clientY;
+      let moved = (nowClientY - clientY) / 3;
+      if (Math.abs(moved) >= threshold) {
+
+      } else {
+        this.setData({
+          translate: `transform:translate(0px,${-moved}px) translateZ(0px);
+          transition: transform 0 linear`
+        })
+      }
+    }
+  },
+  end(e) {
+    this.setData({
+      isReachBottom: false
+    })
+  },
+  onReady() {
+    const query = wx.createSelectorQuery()
+    const clientRect = query.select('page').boundingClientRect((res) => {
+      this.setData({
+        containerBottom: `height:800px`
+      })
+      console.log(res)
+    }).exec()
   }
 })
