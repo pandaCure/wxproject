5a9551c5839a263edb6a9973e54781d7bda5f2c1
Jazk-Z <zzyamor@gmail.com>
2018-12-21 23:21:56 +0800

    微信小程序

--- pages/index/detail/detail.js
+++ pages/index/detail/detail.js
@@ -4,7 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    systemInfo: ''
   },
 
   /**
@@ -18,7 +18,13 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          systemInfo: res
+        })
+      },
+    })
   },
 
   /**
